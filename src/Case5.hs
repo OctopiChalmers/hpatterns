7e4e4749787e6a5ff372ab7d9d9c453d867b1bb2
Thomas Li <guslithd@student.gu.se>
2021-03-05 11:13:24 +0100

    Add Case5 (safer Case4)

--- src/Case5.hs
+++ src/Case5.hs
@@ -1,9 +1,10 @@
-{- | Same as Case1, but with a separate class for sum type
-constructors/branches, not using the Partable class.
+{- | Improvement on Case4, making the case function slighly safer.
 -}
 
-module Case4 where
+module Case5 where
 
+import qualified Data.Map.Strict as M
+import qualified Data.List as List
 import qualified Language.C99.Simple.AST as C
 import qualified Language.C99.Simple
 import qualified Language.C99 as C99
@@ -19,7 +20,7 @@ class (Bounded c, Enum c) => SumCons c where
     cond :: c -> HExp Bool
 
 data T = T1 | T2 | T3
-    deriving (Show, Bounded, Enum)
+    deriving (Show, Bounded, Enum, Eq, Ord)
 
 instance SumCons T where
     cond :: T -> HExp Bool
@@ -27,26 +28,45 @@ instance SumCons T where
     cond T2 = (HPVar `HGt` HVal 0) `HAnd` (HVal 10 `HGt` HPVar)
     cond T3 = (HVal 0 `HGt` HPVar) `HOr` (HPVar `HGt` HVal 10)
 
-case4 :: forall c a b .
+case5 :: forall c a b .
     ( SumCons c
+    , Ord c
+    , Show c
     , Show a
     )
     => HExp a
-    -> [HExp a -> HExp b]
+    -> [(c, HExp a -> HExp b)]  -- ^ Function for each constructor.
     -> HExp b
-case4 scrut fs = HCase0 scrut (zip conditions bodies)
+case5 scrut fs = HCase0 scrut (M.elems matches) -- (zip conditions bodies)
   where
-    bodies :: [HExp b]
-    bodies = map ($ HPVar) fs
+    matches :: M.Map c (HExp Bool, HExp b)
+    matches = go fs M.empty conditions
+      where
+        go ::
+               [(c, HExp a -> HExp b)]
+            -> M.Map c (HExp Bool, HExp b)
+            -> M.Map c (HExp Bool)
+            -> M.Map c (HExp Bool, HExp b)
+        go [] res m =
+            if M.size res == M.size m
+            then res
+            else error
+                $ "case5: mismatch between number of functions and cases, "
+                <> "constructors for cases are: "
+                <> concatMap (("\n" ++) . show) ([minBound ..] :: [c])
+        go ((c, f) : xs) res m =
+            let body = f HPVar :: HExp b
+                cond = m M.! c :: HExp Bool
+            in go xs (M.insert c (cond, body) res) m
 
     constructors :: [c]
     constructors = [minBound ..]
 
-    conditions :: [HExp Bool]
-    conditions = map cond constructors
+    conditions :: M.Map c (HExp Bool)
+    conditions = M.fromList $ map (\ c -> (c, cond c)) constructors
 
-case4ex :: HExp Bool
-case4ex = case4 @T (HVar "e") [zero, small, large]
+case5ex :: HExp Bool
+case5ex = case5 @T (HVar "e") [(T1, zero), (T2, small), (T3, large)]
   where
     zero :: HExp Int -> HExp Bool
     zero = const (HVal True)
@@ -58,23 +78,24 @@ case4ex = case4 @T (HVar "e") [zero, small, large]
     large = const (HVal True)
 
 -- | Replace HPVars with HVars. Does not support nested case-expressions.
-bind4 :: HExp a -> HExp a
-bind4 (HCase0 scrut cases) = HCase0 (HNamedExp "x" scrut) (map bindCase cases)
+bind5 :: HExp a -> HExp a
+bind5 (HCase0 scrut cases) = HCase0 (HNamedExp "x" scrut) (map bindCase cases)
   where
     bindCase :: (HExp Bool, HExp a) -> (HExp Bool, HExp a)
-    bindCase (cond, body) = (bind4 cond, bind4 body)
-bind4 HPVar = HVar "x"  -- hardcoded
-bind4 (HVal v) = HVal v
-bind4 (HVar s) = HVar s
-bind4 (HAdd e1 e2) = HAdd (bind4 e1) (bind4 e2)
-bind4 (HMul e1 e2) = HMul (bind4 e1) (bind4 e2)
-bind4 (HGt e1 e2) = HGt (bind4 e1) (bind4 e2)
-bind4 (HEq e1 e2) = HEq (bind4 e1) (bind4 e2)
-bind4 (HAnd e1 e2) = HAnd (bind4 e1) (bind4 e2)
+    bindCase (cond, body) = (bind5 cond, bind5 body)
+bind5 HPVar = HVar "x"  -- hardcoded
+bind5 (HVal v) = HVal v
+bind5 (HVar s) = HVar s
+bind5 (HAdd e1 e2) = HAdd (bind5 e1) (bind5 e2)
+bind5 (HMul e1 e2) = HMul (bind5 e1) (bind5 e2)
+bind5 (HGt e1 e2) = HGt (bind5 e1) (bind5 e2)
+bind5 (HEq e1 e2) = HEq (bind5 e1) (bind5 e2)
+bind5 (HAnd e1 e2) = HAnd (bind5 e1) (bind5 e2)
+bind5 (HOr e1 e2) = HOr (bind5 e1) (bind5 e2)
 
 -- | Build a C AST representation of an HExp.
-buildAst4 :: Show a => HExp a -> C.TransUnit
-buildAst4 e = wrapper
+buildAst5 :: Show a => HExp a -> C.TransUnit
+buildAst5 e = wrapper
   where
     wrapper :: C.TransUnit
     wrapper = C.TransUnit [] [fd1]
@@ -106,14 +127,15 @@ buildAst4 e = wrapper
         HAdd e1 e2 -> C.BinaryOp C.Add (cExp e1) (cExp e2)
         HGt e1 e2 -> C.BinaryOp C.GT (cExp e1) (cExp e2)
         HAnd e1 e2 -> C.BinaryOp C.LAnd (cExp e1) (cExp e2)
+        HOr e1 e2 -> C.BinaryOp C.LOr (cExp e1) (cExp e2)
         HEq e1 e2 -> C.BinaryOp C.Eq (cExp e1) (cExp e2)
         _ -> error $ "cExp: unexpected expression `" <> show e <> "`"
 
 {- | Use pretty library and pretty instance from language-c99 to print
 somewhat legit-looking C code from a language-c99-simple AST.
 -}
-printAst4 :: Language.C99.Simple.TransUnit -> IO ()
-printAst4 = print . pPrint . Language.C99.Simple.translate
+printAst5 :: Language.C99.Simple.TransUnit -> IO ()
+printAst5 = print . pPrint . Language.C99.Simple.translate
 
 instance Pretty C99.TransUnit where
     pPrint = C99.Pretty.pretty
