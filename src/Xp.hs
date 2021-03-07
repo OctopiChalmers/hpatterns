5e0b1e13d1d1298e7e0384afcb1b6915b1423741
Thomas Li <guslithd@student.gu.se>
2021-03-07 15:24:12 +0100

    Test Xp program

--- src/Xp.hs
+++ src/Xp.hs
@@ -21,3 +21,25 @@ xcase :: forall a b .
     -> (Int -> Xp a -> Xp b)
     -> Xp b
 xcase = undefined
+
+-- | Increment by 1 for positive values, return 0 otherwise.
+xprog1 :: Xp Int
+xprog1 = xcase f g
+  where
+    f :: Xp Int -> [Xp Bool]
+    f sv =
+        [ sv `Gt` Val 0  -- x > 0
+        , Val 0 `Gt` sv  -- 0 > x
+        ]
+
+    g :: Int -> Xp Int -> Xp Int
+    g n = case n of
+        0 -> pos
+        1 -> neg
+        _ -> error "boom"
+
+    pos :: Xp Int -> Xp Int
+    pos sv = sv `Add` Val 1
+
+    neg :: Xp Int -> Xp Int
+    neg _ = Val 0
