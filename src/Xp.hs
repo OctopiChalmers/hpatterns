f9166180fb25be81eadb772ad59c76a7cecad7fd
Thomas Li <guslithd@student.gu.se>
2021-03-07 20:11:55 +0100

    Small syntax improvement

--- src/Xp.hs
+++ src/Xp.hs
@@ -3,6 +3,7 @@ module Xp where
 import qualified Control.Monad.Trans.Reader as R
 
 
+-- | Main data type.
 data Xp a where
     Val :: a -> Xp a
     Var :: String -> Xp a
@@ -20,9 +21,14 @@ data Xp a where
 deriving stock instance Show a => Show (Xp a)
 
 instance Num a => Num (Xp a) where
-    fromInteger e = Val $ fromInteger e
+    fromInteger n = Val (fromInteger n)
     e1 + e2 = Add e1 e2
 
+--
+-- * Combinators
+--
+
+-- | Build a case expression.
 xcase :: forall a b .
     ( Show a
     )
@@ -47,36 +53,48 @@ xcase var g f = Case var (zip conds bodies)
         bodies :: [Xp a -> Xp b]
         bodies = map f [0 .. n]
 
-printC :: Show a => Xp a -> IO ()
-printC e = putStrLn $ R.runReader (toC e) Nothing
+-- | @>@ for Xp.
+(>.) :: (Show a, Num a) => Xp a -> Xp a -> Xp Bool
+(>.) = Gt
 
-toC :: forall a .
+-- | @==@ for Xp.
+(==.) :: (Show a, Eq a) => Xp a -> Xp a -> Xp Bool
+(==.) = Eq
+
+--
+-- * Prettyprinting
+--
+
+pPrintXp :: Show a => Xp a -> IO ()
+pPrintXp e = putStrLn $ R.runReader (prettyXp e) Nothing
+
+prettyXp :: forall a .
     ( Show a
     )
     => Xp a
     -> R.Reader (Maybe String) String  -- Env stores the name of the scrutinee
-toC (Case scrut matches) = do
+prettyXp (Var s) = pure s
+prettyXp (Val v) = pure $ show v
+prettyXp SymVar = R.ask >>= \case
+    Nothing -> error "unknown SymVar reference"
+    Just s -> pure s
+prettyXp (Case scrut matches) = do
     let newVar = "x"  -- hardcoded
-    scrutStr <- toC scrut
-    matchesStrs <- R.local (const $ Just newVar) (mapM toCmatch matches)
+    scrutStr <- prettyXp scrut
+    matchesStrs <- R.local (const $ Just newVar) (mapM prettyXpMatch matches)
     pure $ "int " <> newVar <> " = " <> scrutStr <> ";\n"
         <> unlines matchesStrs <> "\n"
   where
-    toCmatch :: (Xp Bool, Xp a) -> R.Reader (Maybe String) String
-    toCmatch (cond, body) = do
-        condStr <- toC cond
-        bodyStr <- toC body
+    prettyXpMatch :: (Xp Bool, Xp a) -> R.Reader (Maybe String) String
+    prettyXpMatch (cond, body) = do
+        condStr <- prettyXp cond
+        bodyStr <- prettyXp body
         pure $ "if (" <> condStr <> ") { " <> bodyStr <> " }"
-toC (Var s) = pure s
-toC (Val v) = pure $ show v
-toC (Add e1 e2) = do
-    e1Str <- toC e1
-    e2Str <- toC e2
+prettyXp (Add e1 e2) = do
+    e1Str <- prettyXp e1
+    e2Str <- prettyXp e2
     pure $ e1Str <> " + " <> e2Str
-toC (Gt e1 e2) = do
-    e1Str <- toC e1
-    e2Str <- toC e2
+prettyXp (Gt e1 e2) = do
+    e1Str <- prettyXp e1
+    e2Str <- prettyXp e2
     pure $ e1Str <> " > " <> e2Str
-toC SymVar = R.ask >>= \case
-    Nothing -> error "unknown SymVar reference"
-    Just s -> pure s
--- src/XpExample.hs
+++ src/XpExample.hs
@@ -5,22 +5,21 @@ import Xp
 
 -- | Increment by 1 for positive values, return 0 otherwise.
 xprog1 :: Xp Int
-xprog1 = xcase (Var "e") f g
+xprog1 = xcase (3 + 4) f g
   where
     f :: Xp Int -> [Xp Bool]
     f sv =
-        [ sv `Gt` Val 0  -- x > 0
-        , Val 0 `Gt` sv  -- 0 > x
+        [ sv >. 0
+        , 0 >. sv
         ]
 
     g :: Int -> Xp Int -> Xp Int
     g n = case n of
         0 -> pos
         1 -> neg
-        _ -> error "boom"
 
     pos :: Xp Int -> Xp Int
-    pos sv = sv `Add` Val 1
+    pos sv = sv + 1
 
     neg :: Xp Int -> Xp Int
-    neg _ = Val 0
+    neg _ = 0
