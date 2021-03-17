bfd7478f18cd966a46c07f9af413202efc9f1a22
Thomas Li <guslithd@student.gu.se>
2021-03-17 21:09:50 +0100

    Fix examples

--- src/Xp/Examples.hs
+++ src/Xp/Examples.hs
@@ -23,8 +23,8 @@ import Xp.ExamplesTypes
 import Xp.TH
 
 
-printProg :: Show a => Xp a -> IO ()
-printProg = putStrLn . compile
+printProg :: Show a => Hiska (Xp a) -> IO ()
+printProg = putStrLn . compile . runHiska
 
 --
 -- * Example 1
@@ -35,21 +35,23 @@ printProg = putStrLn . compile
 C output:
 
 @
-GHCi> printProg $ ex1 3
+GHCi> printProg $ ex1 4
 
-// Code generated from Xp program
+int coreId0;
 
-int v0(int scrut) {
+int v0(int v2) {
+    coreId0 = v2;
     int v1;
-    if ((scrut > 0)) { v1 = (scrut + 1) }
-    if ((scrut < 0)) { v1 = scrut }
-    if ((scrut == 0)) { v1 = 0 }
+    if ((coreId0 > 0)) { v1 = (coreId0 + 1); } else
+    if ((coreId0 < 0)) { v1 = coreId0; } else
+    if ((coreId0 == 0)) { v1 = 0; } else
+    { printf("Non-exhaustive conditions in function `v0`\n"); }
     return v1;
 }
 
 int main() {
-    int output = v0(3);
-    printf("Program output is: %d", output)
+    int output = v0(4);
+    printf("Program output is: %d\n", output);
     return 0;
 }
 @
@@ -72,44 +74,45 @@ instance (Num a, Show a, Eq a) => Partition Sig a where
 
 {- | Return True if the input char is exactly 'A', False otherwise.
 
-C output (probably doesn't actually run properly due to improper
-string representation in C):
+C output (doesn't actually run due to improper string representation in C):
 
 @
 GHCi> printProg $ ex2 (xval 'A')
 
-// Code generated from Xp program
+int coreId0;
 
-int v0(int scrut) {
+int v0(int v2) {
+    coreId0 = v2;
     int v1;
-    if ((scrut == 'A')) { v1 = 1 }
-    if ((!(scrut == 'A'))) { v1 = 0 }
+    if ((coreId0 == 'A')) { v1 = True; } else
+    if ((!(coreId0 == 'A'))) { v1 = False; } else
+    { printf("Non-exhaustive conditions in function `v0`\n"); }
     return v1;
 }
 
 int main() {
     int output = v0('A');
-    printf("Program output is: %d", output)
+    printf("Program output is: %d\n", output);
     return 0;
 }
 @
 -}
--- ex2 :: Xp Char -> Xp Bool
--- ex2 var = case' var $ \case
---     CharA _    -> xval True
---     CharNotA _ -> xval False
+ex2 :: Xp Char -> Hiska (Xp Bool)
+ex2 var = case' var $ \case
+    CharA _    -> pure $ xval True
+    CharNotA _ -> pure $ xval False
 
--- instance Partition PartChar Char where
---     conds var = [var ==. xval 'A', var /=. xval 'A']
---     constructors = $(makeConstructors ''PartChar)
+instance Partition PartChar Char where
+    partition var = PartitionData preds constructors
+      where
+        preds = [var ==. xval 'A', var /=. xval 'A']
+        constructors = [CharA var, CharNotA var]
 
 --
 -- * Example 3
 --
 
 {- | More complicated example with nested case-expressions.
-
-CURRENTLY DOES NOT BEHAVE CORRECTLY, SEE C OUTPUT BELOW.
 
 In this fictional example, a plant needs watering if one of the following
 conditions apply:
@@ -120,38 +123,39 @@ conditions apply:
 
 C Output:
 
-Note the incorrect behavior caused by nested case expressions; while in
-Haskell we have values from both patterns in scope, the compilation does
-not. NEEDS FIXING.
-
 @
 GHCi> printProg $ needsWatering 25 0.3
 
-// Code generated from Xp program
+int coreId0;
+int coreId1;
 
-int v0(int scrut) {
-    int v1;
-    if ((scrut < 0.2)) { v1 = True }
-    if ((0.2 < scrut)) { v1 = v2(25) }
-    return v1;
+int v2(int v4) {
+    coreId1 = v4;
+    int v3;
+    if ((coreId1 < 18)) { v3 = False; } else
+    if (((18 < coreId1) && (coreId1 < 30))) { v3 = False; } else
+    if ((30 < coreId1)) { v3 = ((coreId1 > 35) && (coreId0 < 0.5)); } else
+    { printf("Non-exhaustive conditions in function `v2`\n"); }
+    return v3;
 }
 
-int v2(int scrut) {
-    int v3;
-    if ((scrut < 18)) { v3 = False }
-    if (((18 < scrut) && (scrut < 30))) { v3 = False }
-    if ((30 < scrut)) { v3 = ((scrut > 35) && (scrut < 0.5)) }
-    return v3;
+int v0(int v5) {
+    coreId0 = v5;
+    int v1;
+    if ((coreId0 < 0.2)) { v1 = True; } else
+    if ((0.2 < coreId0)) { v1 = v2(25); } else
+    { printf("Non-exhaustive conditions in function `v0`\n"); }
+    return v1;
 }
 
 int main() {
     int output = v0(0.3);
-    printf("Program output is: %d", output)
+    printf("Program output is: %d\n", output);
     return 0;
 }
 @
 -}
-needsWatering :: Xp Int -> Xp Moisture -> Hiska (Xp Bool)
+needsWatering :: Xp Temp -> Xp Moisture -> Hiska (Xp Bool)
 needsWatering temp moistLvl = case' moistLvl $ \case
     MoistureOk m -> case' temp $ \case
         TempHot t -> pure $ t >. 35 &&. m <. 0.5
