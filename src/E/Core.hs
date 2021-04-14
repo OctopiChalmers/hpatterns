c104deb4c6578933b81e9c5c9666666c88280cec
Thomas Li <guslithd@student.gu.se>
2021-04-14 02:04:36 +0200

    WIP: Playing with SOP

--- src/E/Core.hs
+++ src/E/Core.hs
@@ -1,14 +1,80 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
 
 module E.Core where
 
+import Data.Kind
+import Data.Void
+
+import Data.SOP
+import Generics.SOP
+
+import qualified Control.Monad.State.Strict as St
+import qualified GHC.Generics as GG
+
+
+data Sig = Pos (E Int) | Neg deriving GG.Generic
+instance Generic Sig
+
+class Generic p => Partition p a where
+    type P p :: [[*]]
+
+    partition :: [E a -> (E Bool, SOP I (P p))]
+
+instance Partition Sig Double where
+    type P Sig = '[ '[E Int] , '[] ]
+
+    partition =
+        [ \ v -> (v >. 0, from $ Pos $ c_floorInt v)
+        , \ v -> (v >. 0, from Neg)
+        ]
+
+case' :: forall p a b . Partition p a
+    => E a
+    -> (SOP I (P p) -> E b)
+    -> Estate (E b)
+case' s f = do
+    scrutVar <- freshId
+
+    let branches = map ($ ESym scrutVar) $ partition @p @a
+    let matches = map (\ (cond, sop) -> Match @p @b cond sop (f sop)) branches
+
+    pure $ ECase (Scrut s scrutVar) matches
+
+ex :: E Double -> Estate (E Int)
+ex v = case' @Sig v $ \case
+    SOP (Z (I n :* Nil)) -> n + 1
+    _ -> undefined
+    -- Neg   -> 0
+
+data Scrut a = Scrut (E a) String
+
+data Match p b where
+    Match
+        :: E Bool
+        -> SOP I (P p)
+        -> E b
+        -> Match p b
 
 data E a where
     EVal :: a -> E a
     EVar :: String -> E a
+
+    ESym :: String -> E a
+    ECase :: Partition p a => Scrut a -> [Match p b] -> E b
 
     EAdd :: (Num a) =>        E a -> E a -> E a
     EMul :: (Num a) =>        E a -> E a -> E a
@@ -20,6 +86,11 @@ data E a where
     ENot ::                   E Bool -> E Bool
     EAnd ::                   E Bool -> E Bool -> E Bool
     EOr  ::                   E Bool -> E Bool -> E Bool
+
+    -- C stuff
+
+    ECFloorInt    :: E Double -> E Int
+    ECFloorDouble :: E Double -> E Double
 
 instance (Num a) => Num (E a) where
     (+) = EAdd
@@ -47,7 +118,7 @@ infix 4 ==.
 
 infix 4 /=.
 (/=.) :: (Eq a) => E a -> E a -> E Bool
-x /=. y = enot (x `EEq` y)
+x /=. y = e_not (x `EEq` y)
 
 infixr 3 &&.
 (&&.) :: E Bool -> E Bool -> E Bool
@@ -57,11 +128,38 @@ infixr 2 ||.
 (||.) :: E Bool -> E Bool -> E Bool
 (||.) = EOr
 
-enot :: E Bool -> E Bool
-enot = ENot
+e_not :: E Bool -> E Bool
+e_not = ENot
 
-evar :: String -> E a
-evar = EVar
+e_var :: String -> E a
+e_var = EVar
 
-eval :: a -> E a
-eval = EVal
+e_val :: a -> E a
+e_val = EVal
+
+
+-- * Uses <math.h>
+
+c_floorDouble :: E Double -> E Double
+c_floorDouble = ECFloorDouble
+
+c_floorInt :: E Double -> E Int
+c_floorInt = ECFloorInt
+
+c_fracPart :: E Double -> E Double
+c_fracPart d = d - c_floorDouble d
+
+
+-- * Auxiliary stuff
+
+type Estate = St.State Int
+
+runEstate :: Estate a -> a
+runEstate x = St.evalState x 0
+
+-- | Return a unique identifier and increment the counter in state.
+freshId :: Estate String
+freshId = do
+    newId <- ("coreId" ++) . show <$> St.get
+    St.modify'(+ 1)
+    pure newId
