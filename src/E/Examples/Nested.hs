d3fbdfefeabd072107509e1fce8e094d06474036
Thomas Li <guslithd@student.gu.se>
2021-08-18 19:47:16 +0200

    Add nested example

---
+++ src/E/Examples/Nested.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE LambdaCase            #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell       #-}
+
+-- | Using nested applications of 'caseof'.
+
+module E.Examples.Nested where
+
+import E.Lang
+import E.TH
+
+
+nested :: E Int -> Estate (E Int)
+nested v = matchM v $ \case
+    T2   -> pure 98
+    T1 x -> matchM (x + 1000) $ \case
+        T2   -> pure 99
+        T1 y -> pure (x + 2 + y)
+
+data T = T1 (E Int) | T2
+$(mkConstructors ''T)
+
+instance Partition Int T where
+    partition = [ \v -> (v >=. 0, _T1 (v * 2))
+                , \v -> (v <. 0, _T2)
+                ]
+
